32082c5030a0773d331eb9ad2fa7160f1eb08165
khtechdeveloper <khtechdeveloper@gmail.com>
2025-05-20 23:45:31 +0530

    Git java

---
+++ Git.java
@@ -0,0 +1,8 @@
+public class Git {
+    public static void main(String[] args) {
+            System.out.println(    "hello java."  );
+            
+System.out.println(    "hello java."  );
+}
+    }
+
